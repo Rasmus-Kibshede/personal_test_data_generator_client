03f8082802e88a1b6ebdfed0aac79a3b6e53c002
Rasmus Kibshede <2740ralle@gmail.com>
2024-01-02 12:48:55 +0100

    done

--- cypress/e2e/1-getting-started/Home.cy.js
+++ cypress/e2e/1-getting-started/Home.cy.js
@@ -1,17 +1,6 @@
 /* eslint-disable no-undef */
 /// <reference types="cypress" />
 
-// Welcome to Cypress!
-//
-// This spec file contains a variety of sample tests
-// for a todo list app that are designed to demonstrate
-// the power of writing tests in Cypress.
-//
-// To learn more about how Cypress works and
-// what makes it such an awesome testing tool,
-// please read our getting started guide:
-// https://on.cypress.io/introduction-to-cypress
-
 describe('Person generator', () => {
   beforeEach(() => {
     cy.visit('http://localhost:5173/');
